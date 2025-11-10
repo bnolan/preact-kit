85ac34c87f7964d3584a89950eeae690f492ef23
Ben Nolan <bnolan@gmail.com>
2025-11-10 13:55:17 +1300

    Update ping.ts

--- templates/api/ping.ts
+++ templates/api/ping.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 
-export default function handler(req: Request, res: Response) {
+export default async function handler(req: Request, res: Response) {
+  await new Promise((resolve) => setTimeout(resolve, 10)); // simulate async
+
   res.json({ message: "pong" });
 }
